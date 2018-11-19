0ee1a44b1866666000f4dd4bac37d573d238e69e
Keenan Gaudio <35779579+keenangaudio@users.noreply.github.com>
2018-11-19 15:11:01 -0700

    added C++ comments

--- CoffeePot_assignment_1_Core0/src/Fill_Water_To_Level_ASM.asm
+++ CoffeePot_assignment_1_Core0/src/Fill_Water_To_Level_ASM.asm
@@ -1,1 +1,26 @@
 /* Demonstrate_Water_Control_ASM.asm */
+/*
+
+void fillCoffeePotToWaterLevel (COFFEEPOT_DEVICE* pCoffeePot, int wLevel){
+	//TODO fix this
+	ShowFunctionStubInformation("fillCoffeePotToWaterLevel");
+	//  Activate water, fill to level (blocking)
+		int currentWaterLevel = CurrentWaterLevel_CPP(pCoffeePot);
+		int neededWater =  wLevel - currentWaterLevel;
+
+		//if (CurrentTemperature_CPP(pCoffeePot) >= 79) neededWater+=10;
+
+		if (neededWater > 255) neededWater = 255;
+		else if (neededWater < 0) neededWater = 0;
+
+		pCoffeePot->waterInFlowRegister  = neededWater;
+	if (CurrentWaterLevel_CPP(pCoffeePot) < wLevel) {
+		if(CurrentWaterLevel_CPP(pCoffeePot) > wLevel*4/5)
+			pCoffeePot->waterInFlowRegister = 1;
+		FastForward_OneSimulationTIC(pCoffeePot);
+	}
+
+	pCoffeePot->waterInFlowRegister = 0;
+}
+
+*/
