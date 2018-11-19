826953faf5c95e2787bf357b760a361cbbe3112d
Keenan Gaudio <35779579+keenangaudio@users.noreply.github.com>
2018-11-19 15:12:30 -0700

    added C++ comments

--- CoffeePot_assignment_1_Core0/src/Activate_Water_Control_ASM.asm
+++ CoffeePot_assignment_1_Core0/src/Activate_Water_Control_ASM.asm
@@ -1,1 +1,16 @@
 /* Activate_Water_Control_ASM.asm */
+/*
+
+void Activate_Water_Control ( COFFEEPOT_DEVICE* pCoffeePot ){
+	ShowFunctionStubInformation("Activate_Water_Control");
+
+	// turn on WATER POWER bit in ctrl reg without changing others
+	//unsigned short int oldCrtl = pCoffeePot->controlRegister;
+	pCoffeePot->controlRegister |= WATER_POWER_ENABLE_BIT;
+
+	CloseChannel_CauseSimulationTIC(pCoffeePot);
+	 FastForward_OneSimulationTIC(pCoffeePot);
+}
+
+*/
+
